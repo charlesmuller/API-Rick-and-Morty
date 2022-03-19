ddb6f7b230ccaff762664974068c95551e216346
charlesmuller <charlesmuller@rede.ulbra.br>
2022-03-18 22:09:19 -0300

    update index.php

--- index.php
+++ index.php
@@ -6,22 +6,26 @@
 </head>
 <body>
 	<?php
+		
 		$url = "http://rickandmortyapi.com/api/character";
+		
 		$resultado = json_decode(file_get_contents($url));
 
-	#	var_dump($resultado);
+	#var_dump($resultado);
 
 		foreach($resultado->results as $personagem){
 			foreach($resultado->results as $genero){
+
 			echo "Nome: " . $personagem->name  . "<br><br>";
+			
 			echo "Status: " . $personagem->status . "<br>";
+			
 			echo "Genero: " . $genero->gender . "<br>";
 
 		}
 		}
 		
 	?>
-	
 </body>
 </html>
 <head>
