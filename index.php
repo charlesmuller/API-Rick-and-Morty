b2e80fc924f7ae2c911d10af2772964263101700
charlesmuller <charlesmuller@rede.ulbra.br>
2022-03-18 22:06:33 -0300

    update

--- index.php
+++ index.php
@@ -6,17 +6,22 @@
 </head>
 <body>
 	<?php
-		$url = "http://rickandmortyapi.com/api/character/";
+		$url = "http://rickandmortyapi.com/api/character";
 		$resultado = json_decode(file_get_contents($url));
-		var_dump($resultado);
-		foreach ($resultado->results as $personagem){
-			echo "Personagem: " . $personagem->name . "<br>";
-			
-			echo "Localização: " . $personagem->location . "<br>";
+
+	#	var_dump($resultado);
+
+		foreach($resultado->results as $personagem){
+			foreach($resultado->results as $genero){
+			echo "Nome: " . $personagem->name  . "<br><br>";
+			echo "Status: " . $personagem->status . "<br>";
+			echo "Genero: " . $genero->gender . "<br>";
+
+		}
 		}
 		
-
 	?>
+	
 </body>
 </html>
 <head>
