259c116b69547e8b85722ab46eb6c50000fe84c3
charlesmuller <charlesmuller@rede.ulbra.br>
2022-03-20 13:29:45 -0300

    update em estilo, index e readme.
    Alterações na interface de estilo e também na forma de exibição de gênero dos personagens.

--- index.php
+++ index.php
@@ -9,46 +9,66 @@
 <body>
 	<h1>API Rick and Morty</h1>
 	<div class="card">
-	<?php
-		$url = "http://rickandmortyapi.com/api/character";
-		$url2 = "http://rickandmortyapi.com/api/episode";
-		
-		$resultado = json_decode(file_get_contents($url));
-		$resultado2 = json_decode(file_get_contents($url2));
+		<?php
 			
-		foreach($resultado->results as $personagem){
-			if ($personagem->id == 1){
+			$url = "http://rickandmortyapi.com/api/character?page=1";
+			$url2 = "http://rickandmortyapi.com/api/episode";
 			
-				echo "Nome: ".($personagem->name)."<br>";
-				echo "Status: ".($personagem->status)."<br>";
-				echo "Planeta: ".($personagem->origin->name)."<br>";
+			$resultado = json_decode(file_get_contents($url));
+			$resultado2 = json_decode(file_get_contents($url2));
 
-				foreach($resultado2->results as $episodio){
-					if($episodio->id == 1){
-						echo "Episódio: ".($episodio->name)."<br>";
-					}
-				}
-			}
-		}
-	?>
+			$troca = null;
+						foreach($resultado->results as $personagem){
+							if($personagem->gender == "Male"){
+								if ($personagem->id == 1){
+		?>
+									<div class="container">
+										<img src="<?=$personagem->image?>"><br>
+									</div>
+		<?php
+									echo "Nome: ".($personagem->name)."<br>";
+									echo "Status: ".($personagem->status)."<br>";
+									echo "Planeta: ".($personagem->origin->name)."<br>";
+
+									foreach($resultado2->results as $episodio){
+										if($episodio->id == 1){
+											echo "Episódio: ".($episodio->name)."<br>";
+										}
+									}
+								}	
+							}
+						}	
+		?>
 	</div>
+	<!--
+	<div class="btn">
+		<input name="$troca" type="submit" value="Próximo personagem">
+	</div>
+	-->
 	<div class="card">
-	<?php
-		foreach($resultado->results as $personagem){
-			if ($personagem->id == 3){
-			
-				echo "Nome: ".($personagem->name)."<br>";
-				echo "Status: ".($personagem->status)."<br>";
-				echo "Planeta: ".($personagem->origin->name)."<br>";
+		<?php
+				foreach($resultado->results as $personagem){
+					if($personagem->gender == "Female"){
+						if ($personagem->id == 3){
+			?>
+							<div class="container">
+								<img src="<?=$personagem->image?>"><br>
+							</div>
+			<?php
+					
+							echo "Nome: ".($personagem->name)."<br>";
+							echo "Status: ".($personagem->status)."<br>";
+							echo "Planeta: ".($personagem->origin->name)."<br>";
 
-				foreach($resultado2->results as $episodio){
-					if($episodio->id == 1){
-						echo "Episódio: ".($episodio->name)."<br>";
+							foreach($resultado2->results as $episodio){
+								if($episodio->id == 6){
+									echo "Episódio: ".($episodio->name)."<br>";
+								}
+							}
+						}
 					}
 				}
-			}
-		}
-	?>
+		?>
 	</div>
 </body>
 </html>
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
-##Criado em PHP##
+## Criado em PHP por Charles Müller da Silva ##
 
 - Para executar em windows:
 1 - Baixar xampp (https://www.apachefriends.org/pt_br/download.html);
 2 - No xampp ativar o apache;
-3 - Inserir os arquivos dentro da pasta htdocs dentro da pasta onde o xampp foi instalado;
+3 - Inserir os arquivos clonados do github dentro da pasta htdoc que está dentro da pasta onde o xampp foi instalado no pc (geralmente no disco C:);
 4 - Abrir o navegador em localhost/ pasta-onde-estão-os-arquiovos e vai rodar.
 
 - Para executar em linux:
